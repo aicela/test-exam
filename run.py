e9004634c24b532cf4a4d1286764ae8d62c2c468
aicela <e244576@edu.sbl.ch>
2022-02-15 08:28:43 +0000

    uhm

--- run.py
+++ run.py
@@ -14,22 +14,61 @@ def load_selectors():
 
 
 # webscraping function
-def my_scraper():
-    # YOUR CODE GOES HERE
-    pass
+    def my_scraper():
+        ## get the URL in a useable form
+    url = "https://www.w3schools.com/cssref/css_selectors.asp"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # select your objects
+    table_rows = [elem for elem in soup.select('.ws-table-all tr')]
 
 
-# define route(s)
-@app.route("/")
-def home():
-    return render_template("index.html")
+    # define filter function
+    def filter_func(elem):
+        return True
+
+    # apply filter function
+    table_rows = list(filter(filter_func, table_rows))
 
 
-@app.route("/css-selectors")
-def css_selectors():
-    return render_template("css-selectors.html", selectors=load_selectors())
+    # create the structure for the json file
+    selectors = []
+
+    for row in table_rows:
+        cells = list(row.select('td'))
+        if cells:
+            entry = {
+                'selector': "placeholder text, which will be overwritten below",
+                'example': cells[1].text,
+                'description': cells[2].text,
+                
+            }
+            # we need the following code beacause not all entries in the first column are text - some are links (a-tag)
+            if cells[0].a:
+                entry['selector'] = cells[0].a.text
+            else:
+                entry['selector'] = cells[0].text
+
+            selectors.append(entry)
+            
+
+    with open("selectors.json", 'w') as f:
+        json.dump(selectors, f) 
+        pass
 
 
-# starts the webserver
-if __name__ == "__main__":
-    app.run()
+    # define route(s)
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+
+    @app.route("/css-selectors")
+    def css_selectors():
+        return render_template("css-selectors.html", selectors=load_selectors())
+
+
+    # starts the webserver
+    if __name__ == "__main__":
+        app.run()
